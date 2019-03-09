9ab295982a191870ee8d3895d71e9da01cbbb39f
Jimmy Le <jimmyle77@hotmail.com>
2019-03-09 13:07:15 -0500

    made a my own branch

---
+++ src/ca/concordia/encs/comp354/controller/IterativeOperativeStrategy.java
@@ -0,0 +1,28 @@
+package ca.concordia.encs.comp354.controller;
+
+import java.util.List;
+
+import ca.concordia.encs.comp354.model.Board;
+import ca.concordia.encs.comp354.model.Coordinates;
+import ca.concordia.encs.comp354.model.ReadOnlyGameState;
+
+/**
+ * With this strategy, an operative picks a strategy by iterative through the list of his team's cards
+ * @author Jimmy Le
+ * 
+ *
+ */
+public class IterativeOperativeStrategy extends AbstractPlayerStrategy implements Operative.Strategy {
+
+    @Override
+    public Coordinates guessCard(Operative owner, ReadOnlyGameState state, Clue clue) {
+        List<Coordinates> guesses = beginTurn(owner, state);
+        return guesses.isEmpty()? null : guesses.remove(0);
+    }
+
+    @Override
+    protected boolean isValidGuess(Player owner, Board board, int x, int y) {
+        return true;
+    }
+
+}
