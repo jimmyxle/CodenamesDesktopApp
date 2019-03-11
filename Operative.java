b103716159bc9820f2d7fb62ee05e2e7dd1505f2
Elie Khoury <khoury.elie96@gmail.com>
2019-03-11 17:39:33 -0400

    Added OperativeWeight Strategy
    Added a strategy that checks the weight of all Codenames, based on the hint given by the SpyMaster, and returns the highest weight.

---
+++ Operative.java
@@ -0,0 +1,78 @@
+package ca.concordia.encs.comp354.controller;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
+import ca.concordia.encs.comp354.model.*;
+
+/**
+ * Operatives are Players that produce guesses from {@link SpyMaster}s' clues.
+ * @author Alex Abrams
+ * @author Nikita Leonidov
+ */
+
+public class Operative extends Player {
+	
+	public interface Strategy {
+	    /**
+	     * Requests a guess from this strategy, given the current game state.
+	     * @param owner  the owning player object
+	     * @param state  a read-only view of the current game state
+	     * @param clue   the last clue produced by this player's spymaster
+	     * @return a guess, or <tt>null</tt> if no more guesses are possible
+	     */
+	    Coordinates guessCard(Operative owner, ReadOnlyGameState state, Clue clue);
+	}
+	
+	private final Strategy strategy;
+	
+	//constructor
+	public Operative(Team team, Strategy strategy) {
+		super(team);
+		this.strategy = Objects.requireNonNull(strategy, "strategy");
+	}
+	
+	Coordinates guessCard(ReadOnlyGameState state, Clue clue) {
+		Coordinates ret = strategy.guessCard(this, state, clue);
+		if (ret==null) {
+		    throw new IllegalStateException("cannot produce another guess");
+		}
+		return ret;
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		List<CodenameWord> codenameWords = Card.createRandomCodenameList(Paths.get("res/25wordswithcommonassociatedwords.txt"));
+		Keycard keycard = Keycard.createRandomKeycard();
+
+	    GameState state = new GameState(new Board(codenameWords, keycard));
+	    Board board = state.boardProperty().get();
+	    //====================
+	    //--------TEST--------
+	    //====================
+	    //print out the card list, then return clue and guess for first 2 red cards
+	    System.out.println("Printing out the game board: ");
+	    System.out.println(state.boardProperty().get().toString());
+	    
+	    SpyMaster spy = new SpyMaster(Team.RED, new RandomSpyMasterStrategy());
+	    Operative op = new Operative(Team.RED, new WeightedOperativeStrategy());
+	    
+	    Clue clue = spy.giveClue(state);
+	    System.out.println("First clue: " + clue);
+	    Coordinates guess = op.guessCard(state, clue);
+	    System.out.println("First guess: " + board.getCard(guess.getX(), guess.getY()).getCodename());
+
+        clue = spy.giveClue(state);
+        System.out.println("Second clue: " + clue);
+        guess = op.guessCard(state, clue);
+        System.out.println("Second guess: " + board.getCard(guess.getX(), guess.getY()).getCodename());
+	    
+	}
+
+
+
+}
+
+
