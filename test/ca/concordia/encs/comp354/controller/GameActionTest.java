b4fb2022756e6ca9d5dc6d8e66e6e4b7b75b6e76
LunchBox <Vickel1993@Hotmail.com>
2019-04-06 19:41:39 -0400

    added GameAction testing

---
+++ test/ca/concordia/encs/comp354/controller/GameActionTest.java
@@ -0,0 +1,37 @@
+package ca.concordia.encs.comp354.controller;
+
+import org.junit.Test;
+
+import ca.concordia.encs.comp354.controller.action.AbstractActionTest;
+import ca.concordia.encs.comp354.model.GameState;
+import ca.concordia.encs.comp354.controller.GameAction;
+
+import static org.junit.Assert.*;
+
+public class GameActionTest extends AbstractActionTest {
+		
+	private GameAction ga;
+	private GameState gameState;
+	
+	
+	//apply() fails when called twice
+	@Test(expected=NullPointerException.class)
+	public void applyCalledTwice() {
+		ga.apply(gameState);
+		ga.apply(gameState);
+	}
+
+
+    //undo() fails if apply() has not been called previously
+	@Test(expected=NullPointerException.class)
+	public void isApplyCalledBeforeUndo() {
+		ga.undo(gameState);
+	}
+	
+    //undo() fails if called twice in a row
+	@Test(expected=NullPointerException.class)
+	public void undoCalledTwice() {
+		ga.undo(gameState);
+		ga.undo(gameState);
+	}		
+}
