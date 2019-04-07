1b7c880dd5cef7220df355504b8bb57bbee02191
cobycohen <cobycohen@192.168.2.21>
2019-04-06 22:55:01 -0400

    newest ClueTest.java

--- test/ca/concordia/encs/comp354/controller/ClueTest.java
+++ test/ca/concordia/encs/comp354/controller/ClueTest.java
@@ -1,15 +1,7 @@
 package ca.concordia.encs.comp354.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import ca.concordia.encs.comp354.model.Board;
-import ca.concordia.encs.comp354.model.CodenameWord;
-import ca.concordia.encs.comp354.model.GameState;
-import ca.concordia.encs.comp354.model.Keycard;
-import ca.concordia.encs.comp354.model.Team;
 import ca.concordia.encs.comp354.controller.Clue;
 
 public class ClueTest {
@@ -19,42 +11,16 @@ public class ClueTest {
 	 * Author: Yaacov Cohen 
 	 * 
 	 */
-	
-	 final GameState      model;
-	 final GameController controller;
-	 Clue 			      clue;
-	 private int x; 
-		    
-	 public ClueTest() {
-		 	    Keycard keycard = Keycard.createRandomKeycard();
-		        List<CodenameWord> words = new ArrayList<>();
-		        for (int i=0; i<25; i++) {
-		            words.add(new CodenameWord("foo", Arrays.asList(new CodenameWord.AssociatedWord("bar", 1))));
-		        }
-		        
-		        model = new GameState(new Board(words, keycard));
-		        controller = new GameController.Builder()
-		                .setRedSpyMaster (new SpyMaster(Team.RED,  new SequentialSpyMasterStrategy()))
-		                .setBlueSpyMaster(new SpyMaster(Team.BLUE, new SequentialSpyMasterStrategy()))
-		                .setRedOperative (new Operative(Team.RED,  new SequentialOperativeStrategy()))
-		                .setBlueOperative(new Operative(Team.BLUE, new SequentialOperativeStrategy()))
-		                .setInitialTurn(Team.RED)
-		                .setModel(model)
-		                .create();
-		        }
-		    
+	    
 
 	/* 
 	This test ensures that the guess provided by the spymaster is not the 
 	empty string, and therefore, must be a sequence of characters.
     */
-	@Test(expected=AssertionError.class)
+	@Test(expected=NullPointerException.class)
 	public void clueConstructorFailsForNullClue() {
-		clue= new Clue ("", 1);
-		if (clue.getWord()=="") {
-			throw new AssertionError("The guess must be a word or character");
-		}
-		assertTrue(clue.getWord() != "");
+		Clue clue= new Clue (null, 1);
+		
 	}
 	
 	/*This test ensures that the number of guesses provided by the spymaster 
@@ -62,13 +28,8 @@ public class ClueTest {
 	*/
 	@Test(expected=IllegalArgumentException.class)
 	public void clueConstructorFailsForGuessCountLessThanOne() {
-		clue= new Clue ("coby", -1);
-		if (clue.getGuesses()<1) {
-			throw new IllegalArgumentException("guesses must be greater than 0");
-		}
-		assertTrue(clue.getGuesses()>=1);
+		Clue clue= new Clue ("coby", -1);
 	}
-	
 }
 	
 
